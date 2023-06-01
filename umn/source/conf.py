536550fcfb98599fa3999905ffdbd34ac467a336
tischrei <tino.schreiber@t-systems.com>
2023-06-01 08:28:56 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -28,6 +28,15 @@ project = 'Scalable File Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/scalable-file-service'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/scalable-file-service/umn/'
+otcdocs_doc_title = 'User Guide'
+otcdocs_doc_type = 'umn'
+otcdocs_service_category = 'storage'
+otcdocs_service_title = 'Scalable File Service'
+otcdocs_service_type = 'sfs'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
